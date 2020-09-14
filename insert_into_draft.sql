22237c1b03ec56e546a222a79dc1a796fdffb6ca
Luke <lukesnow2@gmail.com>
2020-09-14 10:27:32 -0600

    2010 complete

---
+++ insert_into_draft.sql
@@ -0,0 +1,22 @@
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Gabe the Younger',1,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Craig',2,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Luke',3,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Trevor',4,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Gabe the Elder',5,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Israel',6,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Omar',7,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Erik',8,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Nick',9,'Online');
+insert into draft (year, owner, draft_pos, draft_loc)
+values (2020,'Troy',10,'Online');
+
+
