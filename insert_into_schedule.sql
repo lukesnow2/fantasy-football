db1dad018f732d43ba89577388a2f1fa2eb7162e
Luke <lukesnow2@gmail.com>
2020-09-15 21:48:06 -0600

    2011 schedule

--- insert_into_schedule.sql
+++ insert_into_schedule.sql
@@ -1,30 +1,32 @@
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,1,'Craig','Israel','Win',115.4,84.95,0,1);
+values (2011,1,'Omar','Troy','Loss',115.35,173.25,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,2,'Craig','Luke','Win',110.9,98.2,0,1);
+values (2011,2,'Omar','Trevor','Loss',138.0,140.7,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,3,'Craig','Ryan B','Win',150.2,100.35,0,1);
+values (2011,3,'Omar','Erik','Loss',120.2,147.0,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,4,'Craig','Gabe the Elder','Loss',129.55,141.0,0,1);
+values (2011,4,'Omar','Craig','Win',150.7,110.4,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,5,'Craig','Trevor','Win',138.1,117.6,0,1);
+values (2011,5,'Omar','Nick','Loss',76.65,133.3,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,6,'Craig','Erik','Win',109.3,100.95,0,1);
+values (2011,6,'Omar','Gabe the Younger','Loss',108.35,111.5,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,7,'Craig','Nick','Loss',112.15, 142.7,0,1);
+values (2011,7,'Omar','Gabe the Elder','Loss',76.2, 109.95,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,8,'Craig','Kyle','Loss',82.8,168.6,0,1);
+values (2011,8,'Omar','Ryan B','Win',133.25, 119.5,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,9,'Craig','Gabe the Younger','Loss',102.5,141.75,0,1);
+values (2011,9,'Omar','Luke','Loss',132.55,141.15,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,10,'Craig','Israel','Loss',109.5,173.55,0,1);
+values (2011,10,'Omar','Troy','Loss',92.8,115.7,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,11,'Craig','Luke','Loss',79.4,122.55,0,1);
+values (2011,11,'Omar','Trevor','Win',149.8,125.25,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,12,'Craig','Ryan B','Win',148.55,124.5,0,1);
+values (2011,12,'Omar','Erik','Win',143.9,77.3,0,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,13,'Craig','Gabe the Elder','Win',135.5,105.35,0,1);
+values (2011,13,'Omar','Craig','Loss',108.9,162.2,0,1);
 
-select * from schedule where year = 2010 order by id desc;
+select owner, count(1) from schedule where year = 2011 group by owner;
 
 -- delete from schedule where id = '426'
+
+-- update schedule set opponent_score = 140.35 where id = 764
--- insert_playoff_games.sql
+++ insert_playoff_games.sql
@@ -1,36 +1,36 @@
 #first round
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,14,'Gabe the Younger','Craig','Win',145.7,139.95,1,1);
+values (2010,14,'Trevor','Craig','Win',131.85,114.0,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,14,'Craig','Gabe the Younger','Loss',139.95,145.7,1,1);
+values (2010,14,'Craig','Trevor','Loss',114.0,131.85,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,14,'Israel','Luke','Loss',110.2,141.35,1,1);
+values (2010,14,'Ryan B','Gabe the Younger','Win',162.65,87.45,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,14,'Luke','Israel','Win',141.35,110.2,1,1);
+values (2010,14,'Gabe the Younger','Ryan B','Loss',87.45,162.65,1,1);
 
 #semi-finals
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Gabe the Younger','Erik','Win',138.15,114.75,1,1);
+values (2010,15,'Trevor','Troy','Win',149.5,136.35,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Erik','Gabe the Younger','Loss',114.75,138.15,1,1);
+values (2010,15,'Troy','Trevor','Loss',136.35,149.5,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Gabe the Elder','Luke','Win',130.45,123.0,1,1);
+values (2010,15,'Luke','Ryan B','Win',164.75,160.5,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Luke','Gabe the Elder','Loss',123.0,130.45,1,1);
+values (2010,15,'Ryan B','Luke','Loss',160.5,164.75,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Israel','Craig','Win',131.6,94.65,1,1);
+values (2010,15,'Craig','Gabe the Younger','Win',112.4,97.65,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,15,'Craig','Israel','Loss',94.65,131.6,1,1);
+values (2010,15,'Gabe the Younger','Craig','Loss',97.65,112.4,1,1);
 
 #championship week
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,16,'Gabe the Younger','Gabe the Elder','Win',173.65,91.25,1,1);
+values (2010,16,'Trevor','Luke','Win',159.55,114.95,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,16,'Gabe the Elder','Gabe the Younger','Loss',91.25,173.65,1,1);
+values (2010,16,'Luke','Trevor','Loss',114.95,159.55,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,16,'Erik','Luke','Win',109.4,92.9,1,1);
+values (2010,16,'Troy','Ryan B','Win',191.05,126.1,1,1);
 insert into schedule (year,week_num,owner,opponent,result,owner_score,opponent_score,playoff_game,modern_scoring)
-values (2010,16,'Luke','Erik','Loss',92.9,109.4,1,1);
+values (2010,16,'Ryan B','Troy','Loss',126.1,191.05,1,1);
 
 
-select * from schedule where playoff_game = 1 and year = (select max(year) from schedule) order by id desc
+select * from schedule where playoff_game = 1 and year = (select max(year) from schedule) order by id desc;
